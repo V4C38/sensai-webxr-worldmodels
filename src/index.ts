3744effeb663d39f9bee49cd835b3b463e9812bc
eggman28 <eugene.yew@gmail.com>
2026-06-23 08:15:50 +1000

    Updated background
    to BAABAB

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ World.create(document.getElementById("scene-container") as HTMLDivElement, {
 })
   .then((world) => {
     world.camera.position.set(0, 1.5, 0);
-    world.scene.background = new THREE.Color(0xffffff);
+    world.scene.background = new THREE.Color(0xBAABAB);
     world.scene.add(new THREE.AmbientLight(0xffffff, 1.0));
 
     world
